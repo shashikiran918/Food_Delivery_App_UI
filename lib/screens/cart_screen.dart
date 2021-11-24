8f2b1ae3ded0b2910d0e74dc66697b89768cdb9e
shashi kiran katkam <85386298+shashikiran918@users.noreply.github.com>
2021-11-24 13:29:51 +0530

    Add files via upload

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,205 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:food_app/data/data.dart';
+import 'package:food_app/models/order.dart';
+
+class CartScreen extends StatefulWidget {
+  @override
+  _CartScreenState createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  _buildCartItem(Order order) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      height: 170.0,
+      child: Row(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Container(
+                  width: 150.0,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(order.food.imageUrl),
+                          fit: BoxFit.cover),
+                      borderRadius: BorderRadius.circular(15.0)),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          order.food.name,
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.w700),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          order.restaurant.name,
+                          style: TextStyle(
+                              fontSize: 16.0, fontWeight: FontWeight.w700),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 10.0),
+                        Container(
+                          width: 100.0,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              border:
+                                  Border.all(width: 0.8, color: Colors.black)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  '-',
+                                  style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 20.0),
+                              Text(
+                                order.quantity.toString(),
+                                style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              SizedBox(width: 20.0),
+                              GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  '+',
+                                  style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              '\$${order.quantity * order.food.price}',
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double totalPrice = 0;
+    currentUser.cart.forEach(
+        (Order order) => totalPrice += order.quantity * order.food.price);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Cart (${currentUser.cart.length})',
+        ),
+      ),
+      body: ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            if (index < currentUser.cart.length) {
+              Order order = currentUser.cart[index];
+              return _buildCartItem(order);
+            }
+            return Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Estimated Delivery Time:',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '25 mins',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Total Cost',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '\$${totalPrice.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            color: Colors.green[700],
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 80.0,
+                  )
+                ],
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return Divider(height: 1.0, color: Colors.grey);
+          },
+          itemCount: currentUser.cart.length + 1),
+      bottomSheet: Container(
+        height: 80.0,
+        width: MediaQuery.of(context).size.width,
+        decoration:
+            BoxDecoration(color: Theme.of(context).primaryColor, boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, -1),
+            blurRadius: 6.0,
+          )
+        ]),
+        child: Center(
+          child: TextButton(
+            onPressed: () {},
+            child: Text('CHECKOUT'),
+            style: TextButton.styleFrom(
+                primary: Colors.white,
+                textStyle: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0)),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/restraunt_screen.dart
@@ -0,0 +1,209 @@
+
+import 'package:flutter/material.dart';
+import 'package:food_app/models/food.dart';
+import 'package:food_app/models/restaurant.dart';
+import 'package:food_app/widgets/rating_stars.dart';
+
+class RestrauntScreen extends StatefulWidget {
+  final Restaurant restaurant;
+
+  RestrauntScreen({required this.restaurant});
+
+  @override
+  _RestrauntScreenState createState() => _RestrauntScreenState();
+}
+
+class _RestrauntScreenState extends State<RestrauntScreen> {
+  _buildMenuItem(Food menuItem) {
+    return Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            height: 175.0,
+            width: 175.0,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(menuItem.imageUrl), fit: BoxFit.cover),
+                borderRadius: BorderRadius.circular(15.0)),
+          ),
+          Container(
+            height: 175.0,
+            width: 175.0,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Colors.black.withOpacity(0.3),
+                      Colors.black87.withOpacity(0.3),
+                      Colors.black54.withOpacity(0.3),
+                      Colors.black38.withOpacity(0.3),
+                    ],
+                    stops: [
+                      0.1,
+                      0.4,
+                      0.6,
+                      0.9
+                    ])),
+          ),
+          Positioned(
+            bottom: 65.0,
+            child: Column(
+              children: [
+                Text(
+                  menuItem.name,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.2),
+                ),
+                Text(
+                  '\$${menuItem.price}',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.2),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+              bottom: 10.0,
+              right: 10.0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.add),
+                  iconSize: 30.0,
+                  color: Colors.white,
+                  onPressed: () => {},
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Hero(
+                tag: widget.restaurant.imageUrl,
+                child: Image(
+                  height: 220.0,
+                  width: MediaQuery.of(context).size.width,
+                  image: AssetImage(widget.restaurant.imageUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_back_ios),
+                      color: Colors.white,
+                      iconSize: 30.0,
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.favorite),
+                      color: Theme.of(context).primaryColor,
+                      iconSize: 30.0,
+                      onPressed: () => {},
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(widget.restaurant.name,
+                        style: TextStyle(
+                            fontSize: 22.0, fontWeight: FontWeight.w600)),
+                    Text("0.2 miles away",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        )),
+                  ],
+                ),
+                RatingStars(widget.restaurant.rating),
+                SizedBox(
+                  height: 6.0,
+                ),
+                Text(
+                  widget.restaurant.address,
+                  style: TextStyle(fontSize: 18.0),
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FlatButton(
+                  onPressed: () {},
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  color: Theme.of(context).primaryColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Text(
+                    'Reviews',
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  )),
+              TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 30.0),
+                    backgroundColor: Theme.of(context).primaryColor,
+                    primary: Theme.of(context).primaryColor,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                  child: Text(
+                    'Contact',
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  ))
+            ],
+          ),
+          SizedBox(height: 10.0),
+          Center(
+            child: Text(
+              'Menu',
+              style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Expanded(
+              child: GridView.count(
+            crossAxisCount: 2,
+            children: List.generate(widget.restaurant.menu.length, (index) {
+              Food food = widget.restaurant.menu[index];
+              return _buildMenuItem(food);
+            }),
+          ))
+        ],
+      ),
+    );
+  }
+}
